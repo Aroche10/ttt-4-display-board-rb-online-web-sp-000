a46415b2871524531808aade7788f30214817a6f
Guiselly Aroche <guisellyaroche@outlook.com>
2021-02-25 03:29:57 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -10,4 +11,14 @@ board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
 display_board(board)
 
 board = ["  ", "  ", "  ", "  ", "X", "  ", "  ", "  ", "  "]
+=======
+board = ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "]
+def display_board(board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+end
+>>>>>>> c3059e8bf65d6b28100e2a2cae88a0560d136e4e
 display_board(board)
